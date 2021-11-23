4ab4b6bfdfa10849572159c4ed69e0c9ad6aeca0
Tfauves <tfauvelle@gmail.com>
2021-11-22 20:48:24 -0500

    initial commit.

---
+++ src/main/java/com/careerdevs/conqureTheWalk/ConqureTheWalkApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.conqureTheWalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConqureTheWalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConqureTheWalkApplication.class, args);
+	}
+
+}
