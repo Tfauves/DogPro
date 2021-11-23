4f56b60bc871e4a1afa00b1609564e5d213e2d12
Tfauves <tfauvelle@gmail.com>
2021-11-22 21:09:23 -0500

    added deployment folder.

--- src/main/java/com/careerdevs/conqureTheWalk/models/Dog.java
+++ src/main/java/com/careerdevs/conqureTheWalk/models/Dog.java
@@ -12,4 +12,35 @@ public class Dog {
     private Long id;
     private String breed;
     private String energyLvl;
+
+    public Dog() {}
+
+    public Dog(String breed, String energyLvl) {
+        this.breed= breed;
+        this.energyLvl = energyLvl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getEnergyLvl() {
+        return energyLvl;
+    }
+
+    public void setEnergyLvl(String energyLvl) {
+        this.energyLvl = energyLvl;
+    }
 }
