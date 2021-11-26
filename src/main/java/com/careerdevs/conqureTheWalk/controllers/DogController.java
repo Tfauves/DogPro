e5a98856e1575446b6c7bb8b1aa8459d6683b00e
Travis J <tfauvelle@gmail.com>
2021-11-26 12:45:28 -0500

    dog entity.

--- src/main/java/com/careerdevs/conqureTheWalk/controllers/DogController.java
+++ src/main/java/com/careerdevs/conqureTheWalk/controllers/DogController.java
@@ -34,10 +34,23 @@ public class DogController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Dog> upDateDog(@PathVariable Long id, @RequestBody Dog updates) {
+    public @ResponseBody Dog updateDog(@PathVariable Long id, @RequestBody Dog updates) {
         Dog updatedDog = repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-        if (updates.)
+        if (updates.getName() != null) updatedDog.setName(updates.getName());
+        if (updates.getAge() != null) updatedDog.setAge(updates.getAge());
+        if (updates.getWeight() != null) updatedDog.setWeight(updates.getWeight());
+        if (updates.getSex() != null) updatedDog.setSex(updatedDog.getSex());
+        if (updates.getBreed() != null) updatedDog.setBreed(updates.getBreed());
+        if (updates.getEnergyLvl() != null) updatedDog.setEnergyLvl(updates.getEnergyLvl());
 
+        return repository.save(updatedDog);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> destroyDog(@PathVariable long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
     }
 }
