b30abf5c621159eea609e8ead31d144f1834d874
Travis J <tfauvelle@gmail.com>
2021-12-05 20:04:27 -0500

    Entry controller get and post mapped.

---
+++ src/main/java/com/careerdevs/conqureTheWalk/controllers/EntryController.java
@@ -0,0 +1,29 @@
+package com.careerdevs.conqureTheWalk.controllers;
+
+
+import com.careerdevs.conqureTheWalk.models.Entry;
+import com.careerdevs.conqureTheWalk.repositories.EntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/entry")
+public class EntryController {
+    @Autowired
+    private EntryRepository repository;
+
+    @GetMapping
+    public List<Entry> getAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Entry> createEntry(@RequestBody Entry newEntry) {
+        return new ResponseEntity<>(repository.save(newEntry), HttpStatus.CREATED);
+    }
+}
