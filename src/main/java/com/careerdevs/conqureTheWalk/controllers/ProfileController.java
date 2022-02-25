798f20e82d14392e5bc02e12375e2118d744d183
Tfauves <tfauvelle@gmail.com>
2022-02-25 10:39:40 -0500

    delete method by id

--- src/main/java/com/careerdevs/conqureTheWalk/controllers/ProfileController.java
+++ src/main/java/com/careerdevs/conqureTheWalk/controllers/ProfileController.java
@@ -173,5 +173,10 @@ public class ProfileController {
     }
 
 
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> destroyDevById(@PathVariable Long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
+    }
 
 }
