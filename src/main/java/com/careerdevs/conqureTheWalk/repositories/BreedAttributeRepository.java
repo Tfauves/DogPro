fd488c33de6291cebc6ada2114740f73293fd83b
Tranquility Base <tfauvelle@gmail.com>
2022-06-26 17:23:10 -0400

    entry type entity created for entry

--- src/main/java/com/careerdevs/conqureTheWalk/repositories/BreedAttributeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.careerdevs.conqureTheWalk.repositories;
-
-import com.careerdevs.conqureTheWalk.models.BreedAttribute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BreedAttributeRepository extends JpaRepository<BreedAttribute, Long> {
-}
